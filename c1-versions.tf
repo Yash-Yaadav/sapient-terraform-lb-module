5225d35183a8f8c728cd1b85a177060bc806255b
John <gopal1409@gmail.com>
2022-07-10 12:26:49 +0530

    terraform

--- c1-versions.tf
+++ c1-versions.tf
@@ -16,13 +16,21 @@ terraform {
     }    
   }
 }
+data "azurerm_key_vault" "terrakv" {
+  name                = "terrakv" // KeyVault name
+  resource_group_name = "cicd" // resourceGroup
+}
 
+data "azurerm_key_vault_secret" "kvsecret" {
+name = "secret" // Name of secret
+key_vault_id = data.azurerm_key_vault.terrakv.id
+}
 # Provider Block
 provider "azurerm" {
  features {}   
  subscription_id = "b4fd38ca-930c-4e35-a642-d7f7075f428e"
   client_id       = "d7400eae-a48a-4710-9256-d5f16cc12437"
-  client_secret   = "EOzYwgqbkZVLtawjvfsa"
+  client_secret   = data.azurerm_key_vault_secret.terraformjenkins.value
   tenant_id       = "961e4a55-c338-498d-a310-544a2b8c027e"       
 }
 
