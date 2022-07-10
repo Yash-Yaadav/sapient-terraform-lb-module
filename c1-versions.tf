8cf0c39d49fcc910afbcf5e9c25eea1abc93cdc2
John <gopal1409@gmail.com>
2022-07-10 12:29:04 +0530

    terraform

--- c1-versions.tf
+++ c1-versions.tf
@@ -22,7 +22,7 @@ data "azurerm_key_vault" "terrakv" {
 }
 
 data "azurerm_key_vault_secret" "kvsecret" {
-name = "secret" // Name of secret
+name = "terraformjenkins" // Name of secret
 key_vault_id = data.azurerm_key_vault.terrakv.id
 }
 # Provider Block
@@ -30,7 +30,7 @@ provider "azurerm" {
  features {}   
  subscription_id = "b4fd38ca-930c-4e35-a642-d7f7075f428e"
   client_id       = "d7400eae-a48a-4710-9256-d5f16cc12437"
-  client_secret   = data.azurerm_key_vault_secret.terraformjenkins.value
+  client_secret   = data.azurerm_key_vault_secret.kvsecret.value
   tenant_id       = "961e4a55-c338-498d-a310-544a2b8c027e"       
 }
 
